1e0415e56ccecfddab215d083eaf46da8549fafa
roch25 <rochellemartins25@gmail.com>
2022-11-30 14:48:48 +0530

    Add exercise 04

---
+++ exercise-04/script.js
@@ -0,0 +1,18 @@
+const user = {
+    name: "Piyush Sharma",
+    designation: "Senior Software Engineer",
+    company: "Infuse Consulting",
+    hobbies: ["Reading", "Listening to music", "Collecting stamps"]
+}
+
+/**
+ * Use destructuting to log the following
+*/
+
+const printUserProfile = () => {
+    // Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
+    console.log();
+}
+
+printUserProfile()
+
